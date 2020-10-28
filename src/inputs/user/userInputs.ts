f008e39f973bbde3bad61695dbe6973b3d4d1a30
rodzy <49137701+rodzy@users.noreply.github.com>
2020-10-28 12:28:21 -0600

    Added user inputs [On hold roles relations at schema level]

---
+++ src/inputs/user/userInputs.ts
@@ -0,0 +1,23 @@
+import { Field, InputType } from "type-graphql";
+import { Role } from "../../entities/Role";
+
+@InputType()
+export class UserInputs {
+    @Field()
+    username!: string;
+
+    @Field()
+    lastName?: string;
+
+    @Field(() => String)
+    email!: string;
+
+    @Field()
+    password!: string;
+
+    @Field()
+    status!: boolean;
+
+    @Field()
+    role!: Role;
+}
